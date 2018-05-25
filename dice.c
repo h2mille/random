fd57086e4a3896dd31ef7b1253147e298aaa11d4
Hugo van Santen <h2mille@gmail.com>
2018-05-25 13:48:26 +0200

    Update dice.c

--- dice.c
+++ dice.c
@@ -4,7 +4,7 @@
  *from an idea of Heliox
  *https://www.youtube.com/watch?v=S-oBujsoe-Q
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <h2mille@gmail.com wrote this file. As long as you retain this notice you
+ * h2mille@gmail.com wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return Hugo van Santen
  * ----------------------------------------------------------------------------
